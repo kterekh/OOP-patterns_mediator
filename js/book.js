f7a7864ed1b335772b5321a9643709b8a0610683
kterekh <62400448+kterekh@users.noreply.github.com>
2021-01-04 17:58:28 +0200

    initial commit

--- js/book.js
+++ js/book.js
@@ -1,7 +1,15 @@
+const fontColor = 'red'
+const fontWeight = 'bold'
+const displayType = 'flex'
+
+function displayNone (buttons) {
+  buttons.style.display = 'none'
+}
+
 class HorizontalMenu {
   constructor () {
     this.btns = document.querySelectorAll('.author')
-    this.posts = document.querySelectorAll('li')
+    this.uls = document.getElementsByTagName('ul')
     this.dropDown()
   }
 
@@ -10,51 +18,15 @@ class HorizontalMenu {
       btn.addEventListener('click', (e) => {
         switch (e.target.id) {
           case 'fhh':
-            document.getElementById('fhu').style.display = 'flex'
-            document.getElementById('1vl').style.color = 'red'
-            document.getElementById('1vl').style.fontWeight = 'bold'
+            document.getElementById('fhu').style.display = displayType
+            document.getElementById('1hl').style.color = fontColor
+            document.getElementById('1hl').style.fontWeight = fontWeight
             mediator.updateVerticalMenu(e.target.id)
-            this.posts.forEach(post => {
-              post.addEventListener('click', (e) => {
-                  switch (e.target.id) {
-                    case '2hl':
-                      document.getElementById('2hl').style.color = 'red'
-                      document.getElementById('2hl').style.fontWeight = 'bold'
-                      // mediator.showPost(e.target.id)
-                  }
-                }
-              )
-            })
-
             break
           case 'shh':
-            document.getElementById('shu').style.display = 'flex'
-            document.getElementById('4vl').style.color = 'red'
-            document.getElementById('4vl').style.fontWeight = 'bold'
-            mediator.updateVerticalMenu(e.target.id)
-            break
-          case 'thh':
-            document.getElementById('thu').style.display = 'flex'
-            document.getElementById('7vl').style.color = 'red'
-            document.getElementById('7vl').style.fontWeight = 'bold'
-            mediator.updateVerticalMenu(e.target.id)
-            break
-          case 'fvh':
-            document.getElementById('fhu').style.display = 'flex'
-            document.getElementById('1hl').style.color = 'red'
-            document.getElementById('1hl').style.fontWeight = 'bold'
-            mediator.updateVerticalMenu(e.target.id)
-            break
-          case 'svh':
-            document.getElementById('shu').style.display = 'flex'
-            document.getElementById('4hl').style.color = 'red'
-            document.getElementById('4hl').style.fontWeight = 'bold'
-            mediator.updateVerticalMenu(e.target.id)
-            break
-          case 'tvh':
-            document.getElementById('thu').style.display = 'flex'
-            document.getElementById('7hl').style.color = 'red'
-            document.getElementById('7hl').style.fontWeight = 'bold'
+            document.getElementById('shu').style.display = displayType
+            document.getElementById('4hl').style.color = fontColor
+            document.getElementById('4hl').style.fontWeight = fontWeight
             mediator.updateVerticalMenu(e.target.id)
             break
         }
@@ -68,51 +40,22 @@ class VerticalMenu {
   constructor () {
     this.btns = document.querySelectorAll('.author')
     this.dropDown()
+
   }
 
-  dropDown () {
-    this.btns.forEach(btn => {
-      btn.addEventListener('click', function (e) {
-        switch (e.target.id) {
-          case 'fhh':
-            document.getElementById('fvu').style.display = 'flex'
-            document.getElementById('1hl').style.color = 'red'
-            document.getElementById('1hl').style.fontWeight = 'bold'
-            mediator.updateHorizontalMenu(e.target.id)
-            break
-          case 'shh':
-            document.getElementById('svu').style.display = 'flex'
-            document.getElementById('4hl').style.color = 'red'
-            document.getElementById('4hl').style.fontWeight = 'bold'
-            mediator.updateVerticalMenu(e.target.id)
-            break
-          case 'thh':
-            document.getElementById('tvu').style.display = 'flex'
-            document.getElementById('7hl').style.color = 'red'
-            document.getElementById('7hl').style.fontWeight = 'bold'
-            mediator.updateVerticalMenu(e.target.id)
-            break
-          case 'fvh':
-            document.getElementById('fvu').style.display = 'flex'
-            document.getElementById('1vl').style.color = 'red'
-            document.getElementById('1vl').style.fontWeight = 'bold'
-            mediator.updateVerticalMenu(e.target.id)
-            break
-          case 'svh':
-            document.getElementById('svu').style.display = 'flex'
-            document.getElementById('4vl').style.color = 'red'
-            document.getElementById('4vl').style.fontWeight = 'bold'
-            mediator.updateVerticalMenu(e.target.id)
-            break
-          case 'tvh':
-            document.getElementById('tvu').style.display = 'flex'
-            document.getElementById('7vl').style.color = 'red'
-            document.getElementById('7vl').style.fontWeight = 'bold'
-            mediator.updateVerticalMenu(e.target.id)
-            break
-        }
-      })
-    })
+  dropDown (e) {
+    switch (e) {
+      case 'fhh':
+        document.getElementById('fvu').style.display = displayType
+        document.getElementById('1vl').style.color = fontColor
+        document.getElementById('1vl').style.fontWeight = fontWeight
+        break
+      case 'shh':
+        document.getElementById('svu').style.display = displayType
+        document.getElementById('4vl').style.color = fontColor
+        document.getElementById('4vl').style.fontWeight = fontWeight
+        break
+    }
   }
 }
 
@@ -123,7 +66,7 @@ class Screen {
     this.paragraph = document.getElementsByClassName('paragraph')
   }
 
-  displayData () {
+  displayData (e) {
     switch (e) {
       case '2hl' :
         this.title.innerHTML = 'Post 2'
@@ -144,7 +87,6 @@ class Mediator {
   }
 
   showPost (e) {
-    debugger
     this.screen.displayData(e)
   }
 
@@ -158,3 +100,5 @@ class Mediator {
 }
 
 const mediator = new Mediator()
+
+
